e72f57e6a955b27cdee56aaa1cb2d83ccd68eb88
Paul  Colandrea <pdcolandrea@Pauls-MacBook-Air.local>
2022-07-31 23:19:50 -0400

    working socket connection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/dist/shared/lib/utils";
 import { trpc } from "../utils/trpc";
+import { useEffect, useMemo } from "react";
+
+export const isBrowser = typeof window !== "undefined";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
--- src/pages/battle/index.tsx
+++ src/pages/battle/index.tsx
@@ -1,7 +1,40 @@
+import dynamic from "next/dynamic";
+import { useEffect, useMemo, useState } from "react";
+import { isBrowser } from "../_app";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
 function Battle() {
+  const [series, setSeries] = useState<number[]>([]);
+
+  const wsInstance = useMemo(
+    () =>
+      isBrowser
+        ? new WebSocket(
+            "wss://ws.coincap.io/prices?assets=bitcoin",
+            "websocket"
+          )
+        : null,
+    []
+  );
+
+  useEffect(() => {
+    if (wsInstance) {
+      wsInstance.onopen = (event) => {
+        console.log("connected");
+      };
+
+      wsInstance.onmessage = (event) => {
+        let parsedPrice = JSON.parse(event.data);
+        let price = parseFloat(parsedPrice.bitcoin);
+        setSeries((currSeries) => currSeries.concat(price));
+      };
+    }
+  }, []);
+
   return (
     <div className="h-screen bg-background p-20 px-40 ">
-      <div className="p-4  bg-white rounded-sm border border-gray-200 shadow-md dark:bg-primary dark:border-gray-700 w-full  flex-row flex">
+      <div className="p-4 bg-white rounded-sm border border-gray-200 shadow-md dark:bg-primary dark:border-gray-700 w-full  flex-row flex">
         <div className="flex-col ">
           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             BITCOIN PRICE
@@ -16,6 +49,26 @@ function Battle() {
         <div className="flex-col">
           <span>Dasasasas</span>
         </div>
+      </div>
+
+      <div className="flex">
+        <Chart
+          series={[
+            {
+              name: "series-1",
+              data: series,
+            },
+          ]}
+          options={{
+            chart: {
+              id: "basic-line",
+            },
+            stroke: {
+              curve: "smooth",
+            },
+          }}
+          type="line"
+        />
       </div>
     </div>
   );
